62d6551000f06a83299de8feeff60b8e81a599b9
208j <ernurnurzhigit8@gmail.com>
2026-05-11 16:44:04 +0500

    Creating a Pickup object and its Material.

---
+++ 3D Roll_a_ball project/Assets/Scripts/Rotater.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
